5d985efd20f25f6a503e6a038b9c8b2395e8cf08
AnsibleGuy <guy@ansibleguy.net>
2023-10-22 16:02:34 +0200

    updated version parsing/check

---
+++ plugins/module_utils/helper/subps_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.parametrize('cmd, rc, stdout', [
+    ('which mkdir', 0, '/usr/bin/mkdir'),
+    ('some-invalid-command', 1, None),  # soft cmd failure
+])
+def test_process(cmd: str, rc: int, stdout: str):
+    from ansible_collections.ansibleguy.nftables.plugins.module_utils.helper.subps import process
+    result = process(cmd)
+
+    assert result['rc'] == rc
+
+    if stdout is not None:
+        assert result['stdout'].strip() == stdout
+
