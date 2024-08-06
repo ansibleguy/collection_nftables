a2f686a60b36bf82315b4b3a28bfe8e85f552a4a
AnsibleGuy <guy@ansibleguy.net>
2024-08-06 14:20:16 +0200

    update docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,10 +5,10 @@ from datetime import datetime
 project = 'Ansible Collection - NFTables'
 copyright = f'{datetime.now().year}, AnsibleGuy'
 author = 'AnsibleGuy'
-extensions = ['sphinx_rtd_theme']
+extensions = ['piccolo_theme']
 templates_path = ['_templates']
 exclude_patterns = []
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'piccolo_theme'
 html_static_path = ['_static']
 html_logo = 'https://netfilter.org/images/netfilter-logo3.png'
 html_favicon = '_static/img/logo.png'
@@ -20,3 +20,4 @@ source_suffix = {
     '.rst': 'restructuredtext',
 }
 html_theme_options = {}
+html_short_title = 'Ansible NFTables'
