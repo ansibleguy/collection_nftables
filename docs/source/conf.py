45dce0d8cd02e8ef5ed20faba87e2a5104f62b6d
AnsibleGuy <guy@ansibleguy.net>
2024-08-08 22:13:08 +0200

    update docs banner

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ source_suffix = {
     '.rst': 'restructuredtext',
 }
 html_theme_options = {
-    'banner_text': 'Check out <a href="https://github.com/ansibleguy/collection_nftables">the repository on GitHub</a> | '
-                   'Report <a href="https://github.com/ansibleguy/collection_nftables/issues/new/choose">missing/incorrect information or broken links</a>'
+    'banner_text': '<a href="https://github.com/ansibleguy/collection_nftables">Repository on GitHub</a> | '
+                   '<a href="https://github.com/ansibleguy/collection_nftables/issues/new/choose">Report errors</a>'
 }
 html_short_title = 'Ansible NFTables'
